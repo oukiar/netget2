06db399edfdc4936f7fffd188f3fe774c41321c3
oukiar <oukiar@gmail.com>
2014-11-29 20:28:11 -0600

    Movido ScrollBox a las devslib

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ from kivy.properties import StringProperty
 #devslib imports
 from widget3D import Image3D, Widget3D
 from utils import Request, alert, MessageBoxTime, fade_in
+from scrollbox import ScrollBox
 
 #rest of libraries
 import json
@@ -161,33 +162,6 @@ class CenterLog(BoxLayout):
             anim.start(w)
         
         super(CenterLog, self).add_widget(w, index)
-
-class ScrollBox(ScrollView):
-    
-    def __init__(self, **kwargs):
-    
-        self.orientation = kwargs.pop('orientation', 'vertical')
-    
-        super(ScrollBox, self).__init__(**kwargs)
-            
-        if self.orientation == 'vertical':
-            self.layout = BoxLayout(orientation='vertical', size_hint_y=None, spacing=10)
-            self.layout.height = 0
-        else:
-            self.layout = BoxLayout(size_hint_x=None, spacing=10)
-            self.layout.width = 0
-    
-    
-        super(ScrollBox, self).add_widget(self.layout)
-        
-    def add_widget(self, w, index=0):
-        
-        self.layout.add_widget(w)
-        
-        if self.orientation == 'vertical':
-            self.layout.height += (w.height + len(self.layout.children)*(self.layout.spacing/2))
-        else:
-            self.layout.width += (w.width + len(self.layout.children)*(self.layout.spacing/2))
     
 class ResultSearch(Popup):
     
