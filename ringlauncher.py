548c8e85a52cb07dd07c62673abb2bd979d25e40
netget <oukiar>
2014-12-07 12:25:06 -0600

    ringdock updated

--- ringlauncher.py
+++ ringlauncher.py
@@ -6,7 +6,15 @@ from widget3D import Image3D
 
 class Launcher(Image3D):
     def __init__(self, **kwargs):
-        super(Launcher, self).__init__(source='bglauncher.png', pos_z=-500, **kwargs)
+        super(Launcher, self).__init__(source='bglauncher.png', 
+                                        #pos_x=-400, 
+                                        #pos_y=-250, 
+                                        #pos_z=-500, 
+                                        #scale_x=.5, 
+                                        #scale_y=.5, 
+                                        **kwargs)
+        
+        
         
         print 'Launcher image size: ', self.size
 
