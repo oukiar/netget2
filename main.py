77423c62a15d76509fe24ecefc0f74c382ca6d6c
oukiar <oukiar@gmail.com>
2014-12-08 08:31:40 -0600

    Weekend work

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ from kivy.clock import Clock
 from kivy.properties import StringProperty
 
 #devslib imports
-from widget3D import Image3D, Widget3D
+from widget3D import Image3D, Widget3D, Edit3D
 from utils import Request, alert, MessageBoxTime, fade_in, LabelItem
 from scrollbox import ScrollBox
 
