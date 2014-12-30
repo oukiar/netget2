b66814fc14de40bfa61cdf6088115a069838aec5
netget <oukiar>
2014-12-29 22:25:46 -0600

    Login semantic fixes

--- php/nglogin.php
+++ php/nglogin.php
@@ -21,15 +21,21 @@ if(isset($_POST["username"]))
                 //save the device ID
                 $deviceName = $_POST["deviceName"];
                 $deviceIP = $_SERVER['REMOTE_ADDR'];
+                
                 mysql_query("insert into ngDevices(devName, devIP, devLastPing, usrID) values('$deviceName', '$deviceIP', NOW(), $usrID)");
                 
                 $devID = mysql_insert_id();
             }
             else
             {
+                $deviceName = $_POST["deviceName"];
+                $deviceIP = $_SERVER['REMOTE_ADDR'];
+                
                 //try to verify the devID
                 $devID = $_POST["devID"];
             
+                mysql_query("update ngDevices set devIP=$deviceIP, devLastPing=NOW() where devID=$devID");
+                
                 
             }
             
--- php/ngpingstunalive.php
+++ php/ngpingstunalive.php
@@ -2,7 +2,8 @@
 
 include("../cod/php/utils.php");
 
-mysql_query("delete from ngstunservers where lastupdate < DATE_SUB(NOW(), INTERVAL 1 DAY)");
+#for now, never delete, only delete under user demand
+#mysql_query("delete from ngstunservers where lastupdate < DATE_SUB(NOW(), INTERVAL 1 DAY)");
 
 $ip = $_SERVER["REMOTE_ADDR"];
 
