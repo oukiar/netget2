aeb5200a7d3de7e54ceeb069b191f3d261c6103d
oukiar <oukiar@gmail.com>
2015-05-06 08:56:52 -0500

    added gitignore

--- netgetnew/main.py
+++ netgetnew/main.py
@@ -3,11 +3,13 @@ from kivy.uix.popup import Popup
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.properties import ObjectProperty, StringProperty
+from kivy.animation import Animation
 
 from ngvars import NGFactory
 
-from devslib.widget3D import Loading
+from devslib.widget3D import Loading, Widget3D
 
 import md5 #FIXME, USE THE LIBSODIUM HASH
 import json
@@ -16,6 +18,26 @@ import json
 
 
 '''
+
+
+class CenterLog(BoxLayout):
+    def __init__(self, **kwargs):
+        
+        super(CenterLog, self).__init__(orientation='vertical', **kwargs)
+        
+    def add_widget(self, w, index=0):
+        
+        if len(self.children) > 0:
+
+            anim = Animation(opacity=1, height=w.height, duration=1)
+            w.height -= 50
+            w.opacity = 0
+            anim.start(w)
+        
+        super(CenterLog, self).add_widget(w, index)
+
+class NetgetSession(FloatLayout):
+    pass
   
 class Msg(Popup):
     message = StringProperty()
@@ -38,7 +60,7 @@ class NetgetBase(FloatLayout):
     
     def __init__(self, **kwargs):
         super(NetgetBase, self).__init__(**kwargs)
-        self.factory = NGFactory(serverurl='http://104.236.181.245/ngcloud/')
+        self.factory = NGFactory()
     
     def do_signup(self):
         print "Creating account"
@@ -84,6 +106,12 @@ class NetgetBase(FloatLayout):
         self.loading = Loading(source='loading.png')
         self.add_widget(self.loading)
         
+        #screenlog
+        self.boxlogin = CenterLog()
+        self.boxlogin.add_widget(Label(text='[color=EEEEEE]Iniciando sesion ...[/color]', markup=True, font_size=32))
+        self.boxlogin.add_widget(Label(text='[color=FFFFFF]Cancelar inicio de sesion[/color]', markup=True, size_hint_y=None, height=200))
+        self.add_widget(self.boxlogin)
+        
         self.factory.Search(collection='NGUsers', 
                                     conditions={"equalTo":{"Username":self.login.txt_user.text}}, 
                                     callback=self.res_login)
@@ -107,6 +135,8 @@ class NetgetBase(FloatLayout):
         if hashpass.hexdigest() == self.user['Password']:
             self.remove_widget(self.loading)
             self.remove_widget(self.login)
+            
+            self.add_widget(NetgetSession(user=self.user) )
         else:
             Msg(message="Your password does not match, please try again", title="Try again").open()
             self.remove_widget(self.loading)
